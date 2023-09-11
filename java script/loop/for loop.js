293bc8d477921a4250a4fc60608d93ff210affa7
suryanshgarg30 <suryanshgarg.1130@gmail.com>
2023-09-11 18:20:28 +0530

    for loop in js

---
+++ java script/loop/for loop.js
@@ -0,0 +1,22 @@
+ // for loop
+ // initialization condition increment
+ for( let sn = 1;   sn<=10;   sn++)
+ {//block for loop 
+     console.log(sn + "india");//statement
+    }
+
+    //value of sn
+    console.log(sn);//error: referenceError:sn is not defined
+
+
+
+
+    console.log("---------------")
+let sn2;
+for( sn2 = 2 ; sn2<=20; sn2++ )
+{//block for loop
+    console.log(sn2 + "india");//statement
+}
+
+//value of sn2
+console.log(sn2);
