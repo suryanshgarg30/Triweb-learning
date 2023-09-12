db137e504e139cd0574bbb2fc39b71c18e409db4
suryanshgarg30 <suryanshgarg.1130@gmail.com>
2023-09-12 19:19:47 +0530

    digital clock in js

---
+++ java script/digital clock/digital clock.js
@@ -0,0 +1,17 @@
+(function getDigitalTime(){
+    let dt = new Date();
+    let hours = dt.getHours();
+    let minutes = dt.getMinutes();
+    let seconds =  dt.getSeconds();
+
+    let hour12 = hours>12? hours-12 : hours;
+    if(hours == 0){
+        hour12 = "12"
+    }
+    let time="";
+    time += hour12<10?"0"+hour12:""+hour12;
+    time += minutes<10?":0"+minutes:":"+minutes;  //turnary operator
+    time += seconds<10?":0"+seconds:":"+seconds;
+    time += hours>12? " PM" : " AM";
+    console.log(time);
+})()
